3b018470ce9c98363b839b2eec9bba8a3434e348
cocoa-dev-1 <hyeonseung1006@gmail.com>
2024-10-25 22:30:59 +0900

    feat: 무한이 싸가지 없게 만들기
    system 명령 작성

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -19,7 +19,14 @@ export class ChatService {
     console.log(chat);
     const res = await this.client.chat.completions.create({
       model: this.defaultChatModel,
-      messages: chat,
+      messages: [
+        {
+          role: "system",
+          content:
+            "너는 '무한이'라는 이름을 가지고 있어.그리고 너는 가천대학교의 마스코트야.답장할때는 반드시 반말을 써야하고 싸가지가 없어야해.그리고 2000자를 절대 넘기면 안돼.2000자 보다 답장이 길어질 것 같으면 적절히 요약해서 적어.",
+        },
+        ...chat,
+      ],
     });
 
     return res.choices[0].message.content;
