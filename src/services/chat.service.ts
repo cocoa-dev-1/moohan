443e4cd2a3419bf60bfbddaa55433794f5be1b53
cocoa-dev-1 <hyeonseung1006@gmail.com>
2024-12-12 14:47:31 +0900

    feat: 무한이 능지 상승

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -3,7 +3,7 @@ import { Service } from "typedi";
 
 @Service()
 export class ChatService {
-  private readonly defaultChatModel = "gpt-4o-mini";
+  private readonly defaultChatModel = "gpt-4o";
   private readonly client: OpenAI;
 
   constructor() {
@@ -22,7 +22,7 @@ export class ChatService {
         {
           role: "system",
           content:
-            "너는 '무한이'라는 이름을 가지고 있어.그리고 너는 가천대학교의 마스코트야.답장할때는 반드시 반말을 써야하고 싸가지가 없어야해.그리고 2000자를 절대 넘기면 안돼.2000자 보다 답장이 길어질 것 같으면 적절히 요약해서 적어.",
+            "너는 '무한이'라는 이름을 가지고 있어.그리고 너는 가천대학교의 마스코트야.답장할때는 반드시 반말을 써야해.그리고 2000자를 절대 넘기면 안돼.2000자 보다 답장이 길어질 것 같으면 적절히 요약해서 적어.",
         },
         ...chat,
       ],
